6a11623efd7b6c8ce511bfab935079a0171dcdc5
Bavith <suvarnabavith@gmail.com>
2025-07-20 22:06:48 +0530

    Removed Certifications

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
       <About />
       <Projects />
       <Technologies />
-      <Certifications /> 
+      {/* <Certifications />  */}
       <Achievements />
       <Contact />
       
