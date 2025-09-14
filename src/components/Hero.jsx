21832b3e7135847620daca67210349167fd8ec21
Bavith <suvarnabavith@gmail.com>
2025-09-14 12:11:09 +0530

    Profile Picture Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/BavithSuvarnaProfile1.jpg";
+import profilePic from "../assets/BavithSuvarnaProfile.png";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
