5631aa121acef197790d024097349ed6f3219792
Bavith <suvarnabavith@gmail.com>
2026-03-19 17:44:17 +0530

    New Skills added for Anvelos

--- src/constants/index.js
+++ src/constants/index.js
@@ -27,7 +27,7 @@ export const EXPERIENCES = [
     company: "Anvelos Softwares",
     image: experience1,
     description: `Working as a Software Engineer Intern at Anvelos Softwares, contributing to the development of innovative software solutions.`,
-    technologies: ["Next.js"],
+    technologies: ["Next.js", "AWS Cognito", "AWS S3"],
     companyUrl: "https://www.anvelos.com/",
   },
   {
