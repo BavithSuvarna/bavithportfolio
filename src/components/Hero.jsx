5c16439f24c7b6e68b668fc1cbb0b2703e2d1b00
Bavith <suvarnabavith@gmail.com>
2026-04-18 11:49:07 +0530

    Added new profile picture

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/BavithSuvarnaProfile.png";
+import profilePic from "../assets/profile.jpeg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
