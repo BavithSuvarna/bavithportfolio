e18b591276cb741254c0e147f3b94152ffff707c
Bavith <suvarnabavith@gmail.com>
2026-07-25 21:24:48 +0530

    Updated profile pic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/profile.jpeg";
+import profilePic from "../assets/profile.png";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
